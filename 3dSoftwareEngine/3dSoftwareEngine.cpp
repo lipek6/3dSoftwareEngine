fa9f7977a9297c7776f7b103cf0b7a3225673a0a
fald <felipeaugustolira2006@gmail.com>
2026-01-27 09:45:06 -0300

    DONE: 3D vectors, triangles and mesh base structs defined. Cube coordinates representation created. ON PROGRESS: Create a matrix struct to allow matrix multiplication and the usage of the projection matrix.

--- 3dSoftwareEngine/3dSoftwareEngine.cpp
+++ 3dSoftwareEngine/3dSoftwareEngine.cpp
@@ -1,9 +1,43 @@
 #include "olcConsoleGameEngine.h"
 
+// STRUCTS ===================================================================================
+
+// 3 Dimensional vector 
+struct vec3d
+{
+    float x, y, z;
+};
 
 
+// Set of 3 Dimensional vectors used to represent triangles
+struct triangle
+{
+    vec3d point[3];
+};
+
+
+// Set of triangles used to Represent the objects.
+struct mesh             
+{
+    std::vector<triangle> vTriangle;
+};
+
+
+// 
+struct matrix4x4
+{
+
+};
+
+
+
+
+// GAME ENGINE CLASS =========================================================================
 class Engine3D : public olcConsoleGameEngine
 {
+private:
+    mesh meshCube;
+
 public:
     Engine3D()
     {
@@ -12,11 +46,48 @@ public:
 
     bool OnUserCreate() override 
     {
+        meshCube.vTriangle = {
+            // South face
+            { (0.0f, 0.0f, 0.0f)   ,   (0.0f, 1.0f, 0.0f)   ,   (1.0f, 1.0f, 0.0f) },
+            { (0.0f, 0.0f, 0.0f)   ,   (1.0f, 1.0f, 0.0f)   ,   (1.0f, 0.0f, 0.0f) },
+
+            // East face
+            { (1.0f, 0.0f, 0.0f)   ,   (1.0f, 1.0f, 0.0f)   ,   (1.0f, 1.0f, 1.0f) },
+            { (1.0f, 0.0f, 0.0f)   ,   (1.0f, 1.0f, 1.0f)   ,   (1.0f, 0.0f, 1.0f) },
+
+            // North face
+            { (1.0f, 0.0f, 1.0f)   ,   (1.0f, 1.0f, 1.0f)   ,   (0.0f, 1.0f, 1.0f) },
+            { (1.0f, 0.0f, 1.0f)   ,   (0.0f, 1.0f, 1.0f)   ,   (0.0f, 0.0f, 1.0f) },
+
+            // West face
+            { (0.0f, 0.0f, 1.0f)   ,   (0.0f, 1.0f, 1.0f)   ,   (0.0f, 1.0f, 0.0f) },
+            { (0.0f, 0.0f, 1.0f)   ,   (0.0f, 1.0f, 0.0f)   ,   (0.0f, 0.0f, 0.0f) },
+
+            // Top face
+            { (0.0f, 1.0f, 0.0f)   ,   (0.0f, 1.0f, 1.0f)   ,   (1.0f, 1.0f, 1.0f) },
+            { (0.0f, 1.0f, 0.0f)   ,   (1.0f, 1.0f, 1.0f)   ,   (1.0f, 1.0f, 0.0f) },
+
+            // Bottom face
+            { (1.0f, 0.0f, 1.0f)   ,   (0.0f, 0.0f, 1.0f)   ,   (0.0f, 0.0f, 0.0f) },
+            { (1.0f, 0.0f, 1.0f)   ,   (0.0f, 0.0f, 0.0f)   ,   (1.0f, 0.0f, 0.0f) }
+        };
+        
+        
         return true; 
     }
     
     bool OnUserUpdate(float fElapsedTime) override
     { 
+        Fill(0, 0, m_nScreenWidth, m_nScreenHeight, PIXEL_SOLID, FG_BLACK);
+        
+        for (const triangle& tri : meshCube.vTriangle)
+        {
+
+        }
+        
+        
+        
+        
         return true; 
     }
 };
@@ -30,6 +101,4 @@ int main()
         demo.Start();
     else
         return -1;
-
-    std::cout << "Hello World!\n";
 }
